f764d81b9a7ea04bd88561ccc8d326bb3cfdf23e
Johannes Rosskopp <johannes.rosskopp@prodyna.com>
2025-09-25 14:54:46 +0000

    website extra function

--- quickstart-azure-go/main.go
+++ quickstart-azure-go/main.go
@@ -1,88 +1,30 @@
 package main
 
 import (
-	// "fmt"
+	"fmt"
 
 	"github.com/pulumi/pulumi-command/sdk/go/command/local"
 
 	resources "github.com/pulumi/pulumi-azure-native-sdk/resources/v3"
-	// storage "github.com/pulumi/pulumi-azure-native-sdk/storage/v3"
-	// web "github.com/pulumi/pulumi-azure-native-sdk/web/v3"
-	// "github.com/pulumi/pulumi-docker/sdk/v4/go/docker"
+	storage "github.com/pulumi/pulumi-azure-native-sdk/storage/v3"
+	web "github.com/pulumi/pulumi-azure-native-sdk/web/v3"
+	"github.com/pulumi/pulumi-docker/sdk/v4/go/docker"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
-	// "github.com/pulumi/pulumi/sdk/v3/go/pulumi/config"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi/config"
 )
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 
-		// myname := "johannes"
+		myname := "johannes"
 
-		
-		random, err := local.NewCommand(ctx, "my-bucket", &local.CommandArgs{
-			Create: pulumi.String("echo 'New File' > new_file.txt; echo $(pwd)/new_file.txt"),
-			Delete: pulumi.String("rm new_file.txt"),
-		})
-		if err != nil {
-			return err
-		}
-
-		ctx.Export("output", random.Stdout)
-
-		_, err = resources.NewResourceGroup(ctx, "my-resource-group", &resources.ResourceGroupArgs{
+		rg, err := resources.NewResourceGroup(ctx, "my-resource-group", &resources.ResourceGroupArgs{
 			Location:          pulumi.String("westeurope"),
 			ResourceGroupName: pulumi.String("pulumi-tech"),
 		}, pulumi.Protect(true))
 		if err != nil {
 			return err
 		}
-/*
-		storageAccount, err := storage.NewStorageAccount(ctx, "storageaccount", &storage.StorageAccountArgs{
-			ResourceGroupName: rg.Name,
-			Location:          rg.Location,
-			Sku: &storage.SkuArgs{
-				Name: pulumi.String("Standard_LRS"),
-			},
-			Kind:        pulumi.String("StorageV2"),
-			AccountName: pulumi.String(fmt.Sprintf("pulumistorage%s", myname)),
-		})
-		if err != nil {
-			return err
-		}
-
-		staticWebsiteIndexDocument := "index.html"
-		staticWebsiteIndexDocumentPath := fmt.Sprintf("./www/%s", staticWebsiteIndexDocument)
-		staticWebsite, err := storage.NewStorageAccountStaticWebsite(ctx, "storageAccountStaticWebsiteResource", &storage.StorageAccountStaticWebsiteArgs{
-			AccountName:       storageAccount.Name,
-			ResourceGroupName: rg.Name,
-			IndexDocument:     pulumi.String(staticWebsiteIndexDocument),
-		})
-		if err != nil {
-			return err
-		}
-
-		_, err = storage.NewBlob(ctx, "blobResource", &storage.BlobArgs{
-			AccountName:       storageAccount.Name,
-			ContainerName:     staticWebsite.ContainerName,
-			ResourceGroupName: rg.Name,
-			// AccessTier:        storage.BlobAccessTierHot,
-			BlobName:    pulumi.String(staticWebsiteIndexDocument),
-			ContentType: pulumi.String("text/html"),
-			Source:      pulumi.NewFileAsset(staticWebsiteIndexDocumentPath),
-			Type:        storage.BlobTypeBlock,
-		})
-		if err != nil {
-			return err
-		}
-
-		// Q: What happens if we print a string.Output directly?
-		// A: ...
-		fmt.Printf("Wrong: You can access your website at the following URL: %s\n", storageAccount.PrimaryEndpoints.Web())
-		// Solution:
-		storageAccount.PrimaryEndpoints.Web().ApplyT(func(url string) error {
-			fmt.Printf("S1: You can access your website at the following URL: %s\n", url)
-			return nil
-		})
 
 		// Setup Docker image: Configure Docker Hub credentials via config
 		// pulumi config set dockerhub:username <your-username>
@@ -115,7 +57,6 @@ func main() {
 				Name: pulumi.String("B1"),
 				Tier: pulumi.String("Basic"),
 			},
-			// Kind: pulumi.String("app,linux,container"),
 			// Linux is true, why oh why, no one knows
 			Reserved: pulumi.BoolPtr(true),
 		})
@@ -148,12 +89,82 @@ func main() {
 			return err
 		}
 
-		// Run a docker build
+		apiUrl := pulumi.Sprintf("http://%s", webapp.DefaultHostName)
 
-		ctx.Export("primaryWebEndpoint", storageAccount.PrimaryEndpoints.Web())
-
-		ctx.Export("apiUrl", pulumi.Sprintf("http://%s", webapp.DefaultHostName))*/
+		ctx.Export("apiUrl", apiUrl)
 
 		return nil
 	})
+}
+
+type StaticWebsiteArgs struct {
+	rg    *resources.ResourceGroup
+	apiUrl pulumi.StringOutput
+}
+
+func createStaticWebsite(ctx *pulumi.Context, args StaticWebsiteArgs) error {
+	config := config.New(ctx, "staticwebsite")
+	myname := config.Require("name")
+
+	storageAccount, err := storage.NewStorageAccount(ctx, "storageaccount", &storage.StorageAccountArgs{
+		ResourceGroupName: args.rg.Name,
+		Location:          args.rg.Location,
+		Sku: &storage.SkuArgs{
+			Name: pulumi.String("Standard_LRS"),
+		},
+		Kind:        pulumi.String("StorageV2"),
+		AccountName: pulumi.String(fmt.Sprintf("pulumistorage%s", myname)),
+	})
+	if err != nil {
+		return err
+	}
+	staticWebsiteIndexDocument := "index.html"
+	// staticWebsiteIndexDocumentPath := fmt.Sprintf("./www/%s", staticWebsiteIndexDocument)
+	staticWebsite, err := storage.NewStorageAccountStaticWebsite(ctx, "storageAccountStaticWebsiteResource", &storage.StorageAccountStaticWebsiteArgs{
+		AccountName:       storageAccount.Name,
+		ResourceGroupName: args.rg.Name,
+		IndexDocument:     pulumi.String(staticWebsiteIndexDocument),
+	})
+	if err != nil {
+		return err
+	}
+
+	staticPageHTML, err := local.NewCommand(ctx, "my-bucket", &local.CommandArgs{
+		Create: pulumi.String("sed -e \"s/API_URL/$ENV_API_URL/\""),
+		Environment: pulumi.StringMap{
+			"ENV_API_URL": args.apiUrl,
+		},
+	})
+	if err != nil {
+		return err
+	}
+
+	staticPageHTML.Stdout.ApplyT(func(staticPageHTML string) error {
+		_, err = storage.NewBlob(ctx, "blobResource", &storage.BlobArgs{
+			AccountName:       storageAccount.Name,
+			ContainerName:     staticWebsite.ContainerName,
+			ResourceGroupName: args.rg.Name,
+			BlobName:          pulumi.String(staticWebsiteIndexDocument),
+			ContentType:       pulumi.String("text/html"),
+			Source:            pulumi.NewStringAsset(staticPageHTML),
+			Type:              storage.BlobTypeBlock,
+		})
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+
+	// Q: What happens if we print a string.Output directly?
+	// A: ...
+	fmt.Printf("Wrong: You can access your website at the following URL: %s\n", storageAccount.PrimaryEndpoints.Web())
+	// Solution:
+	storageAccount.PrimaryEndpoints.Web().ApplyT(func(url string) error {
+		fmt.Printf("S1: You can access your website at the following URL: %s\n", url)
+		return nil
+	})
+
+	ctx.Export("primaryWebEndpoint", storageAccount.PrimaryEndpoints.Web())
+
+	return nil
 }
